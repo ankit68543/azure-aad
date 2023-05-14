fae1bf63b5070317521dc4ed4b02a7a4da1fbab1
Ankit68543 <ankit68543@gmail.com>
2023-05-14 21:17:49 +0530

    Remove credntials

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     msalConfig: {
         auth: {
-            clientId: 'fda7ce73-ab0b-458e-a0a8-f5e22608a768',
-            authority: 'https://login.microsoftonline.com/c60121a4-b833-419a-b94b-2189095e7c15'
+            clientId: 'ENTER_CLIENT_ID',
+            authority: 'https://login.microsoftonline.com/ENTER_AUTHORITY'
         }
     },
     apiConfig: {
--- src/environments/environment.e2e.ts
+++ src/environments/environment.e2e.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     msalConfig: {
         auth: {
-            clientId: '3fba556e-5d4a-48e3-8e1a-fd57c12cb82e',
-            authority: 'https://login.windows-ppe.net/common'
+            clientId: 'ENTER_CLIENT_ID',
+            authority: 'https://login.windows-ppe.net/ENTER_AUTHORITY'
         }
     },
     apiConfig: {
